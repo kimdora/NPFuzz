26b1ab71f83a9f34f65e386865d728f50051f2e0
saykim0727 <saykim0727@naver.com>
2019-05-28 01:37:23 -0700

    Add request class & Add parser operation

--- src/SwaggerParser/SwaggerParser.py
+++ src/SwaggerParser/SwaggerParser.py
@@ -1,10 +1,71 @@
 import yaml
+# integer, string, array, boolean
+class Request():
+  def __init__(self, request):
+    self.basepath = request["basePath"]
+    self.path = request["path"]
+    self.schema = request["schemes"]
+    self.method = request["method"]
+    self.contentType = request["contentType"]
+    self.parameter = request["parameter"]
+    self.reqParam = request["pathParam"]
+    self.response = request["response"]
+    self.dependency = request["dependency"]
+
+  def getBasePath(self):
+    return self.basePath
+
+  def getSchema(self):
+    return self.schema
+
+  def getPathParam(self):
+    return self.pathParam
+
+  def getPath(self):
+    return self.path
+
+  def getMethod(self):
+    return self.method
+
+  def getContentType(self):
+    return self.contentType
+
+  def getParameter(self):
+    return self.parameter
+
+  def getReqParam(self):
+    return self.reqParam
+
+  def getResponse(self):
+    return self.response
 
 class SwaggerParser():
   datalist = {}
   preDef = {}
   def __init__(self, fName):
     self.doc = yaml.load(open(fName,"r"))
+
+  def getReqSet(self):
+    reqSet = []
+    paths = self.getPath()
+    for i in paths:
+      request = {}
+      method = parser.getMethod(i)
+      for j in method:
+        resp = parser.getMethodResponse(j)
+        param, pathParam, dependency = self.getMethodParam(j)
+        produces = self.getProduce(j)
+        request["basePath"] = self.getBasePath()
+        request["schemes"] = self.getSchemes()
+        request["path"] = i
+        request["method"] = j
+        request["contentType"] = produces
+        request["parameter"] = param
+        request["pathParam"] = pathParam
+        request["response"] = resp
+        request["dependency"] = dependency
+        reqSet.append(Request(request))
+    return reqSet
 
   def getBasePath(self):
     return self.doc["basePath"] # String
@@ -30,23 +91,23 @@ class SwaggerParser():
 
   def getProperties(self, properties):
     # TODO : Consider whether default = false
-    ret = {False: {}, True: {}}
+    ret = {"Optional": {}, "Require": {}}
     reqList = []
     if "required" in properties:
       for i in properties["required"]:
         reqList.append(i)
     for i in properties["properties"]:
       if i in reqList:
-        ret[True][i] = self.typeExtract(properties["properties"][i])
+        ret["Require"][i] = self.typeExtract(properties["properties"][i])
       else:
-        ret[False][i] = self.typeExtract(properties["properties"][i])
+        ret["Optional"][i] = self.typeExtract(properties["properties"][i])
     return ret
 
   def refExtract(self, refClass, refFunc):
     funcs = self.doc[refClass][refFunc]
     self.preDef[refClass] = {}
     self.preDef[refClass][refFunc] = self.getProperties(funcs)
-    return "%s[%s]" % (refClass, refFunc)
+    return self.getProperties(funcs)
 
   def typeExtract(self, paramData):
     #TODO : consider "additionalProperties" as items
@@ -64,42 +125,49 @@ class SwaggerParser():
     elif "schema" in paramData:
       ret = self.typeExtract(paramData["schema"])
     else:
-      raise TypeExtractException
+      ret = None
     return ret
+
+  def getProduce(self, method):
+    path = self.datalist[method]
+    if "produces" not in path[method]:
+      return None
+    return path[method]["produces"]
 
   def getMethodParam(self, method):
     path = self.datalist[method]
     if "parameters" not in path[method]:
-      return None
+      return None, None, None
     param = path[method]["parameters"]
-    ret = {False: {}, True: {}}
+    ret = {"Optional": {}, "Require": {}}
+    dependency = {}
+    pathParam = {}
     # TODO : Consider parameters format
-    # Now : { False: { in: type }, True: { in: type } }
+    # Now : { Optional: { in: type }, Require: { in: type } }
     for data in param:
       vType = self.typeExtract(data)
-      if data["required"] == True:
-        ret[True][data["in"]] = vType
+      if data["in"] == "path":
+        pathParam[data["name"]] = vType
+        dependency[data["name"]] = vType
       else:
-        ret[False][data["in"]] = vType
-    return ret # { False: { in: type }, True: { in: type } }
+        if data["required"] == True:
+          ret["Require"][data["name"]] = vType
+          dependency[data["name"]] = vType
+        else:
+          ret["Optional"][data["name"]] = vType
+    return ret, pathParam, dependency
 
   def getMethodResponse(self, method):
-    resp = None
+    ret = {}
     path = self.datalist[method]
     if "responses" in path[method]:
       resp = path[method]["responses"].keys()
+      for i in resp:
+        ret[i] = self.typeExtract(i)
     # TODO : Consider reponses schema
-    return resp
+    return ret
 
 # TODO : Remove if we dont need testing
 if __name__ == "__main__":
   parser = SwaggerParser("swagger.yaml")
-  paths = parser.getPath()
-  for i in paths:
-    pathparam = parser.getPathParam(i)
-    method = parser.getMethod(i)
-    for j in method:
-      resp = parser.getMethodResponse(j)
-      param = parser.getMethodParam(j)
-
-
+  parser.getReqSet()
