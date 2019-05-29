23fc223aa01d763d27530c710e2c92b7a810f2b2
saykim0727 <saykim0727@naver.com>
2019-05-28 20:05:17 -0700

    Add other type

--- src/swagger/parser.py
+++ src/swagger/parser.py
@@ -29,8 +29,8 @@ class SwaggerParser():
         request["response"] = resp
         request["dependency"] = dependency
         req_set.append(Request(request))
-    #for i in req_set:
-    #  print i.response
+    for i in req_set:
+      print (i.dependency)
     return req_set
 
   def get_base_path(self):
@@ -75,23 +75,61 @@ class SwaggerParser():
     self.pre_def[ref_class][ref_func] = self.get_properties(funcs)
     return self.pre_def[ref_class]
 
+  def add_type(self, data, param):
+    if data == "":
+      ret = param
+    elif param == "":
+      ret = data
+    else:
+      ret = "%s*%s" % (data, param)
+    return ret
+
+  def extract_types(self, types):
+    ret = []
+    for i in types:
+      ret = "%s*%s" % (ret, i)
+    return ret
+
   def extract_type(self, param_data):
     #TODO : consider "additionalProperties" as items
     ret = ""
     # type, schema->type, schema->ref, schema->type * (item -> ref), ...
-    if "type" in param_data and "items" in param_data:
-      # main type * item type
-      item = self.extract_type(param_data["items"])
-      ret = "%s*%s" % (param_data["type"], item)
-    elif "type" in param_data:
+    if "type" in param_data:
       ret = param_data["type"]
-    elif "$ref" in param_data:
+    if "schema" in param_data:
+      schema = self.extract_type(param_data["schema"])
+      ret = self.add_type(ret, schema)
+    if "$ref" in param_data:
+      # TODO : $ref is only one?
+      print (len(param_data["$ref"]))
       refer = param_data["$ref"].split("/")
-      ret = self.extract_ref(refer[1],refer[2])
-    elif "schema" in param_data:
-      ret = self.extract_type(param_data["schema"])
-    else:
-      ret = ""
+      reference = self.extract_ref(refer[1],refer[2])
+      ret = self.add_type(ret, reference)
+    if "items" in param_data:
+      item = self.extract_type(param_data["items"])
+      ret = self.add_type(ret, item)
+    if "properties" in param_data:
+      prop_set = {}
+      prop = param_data["properties"]
+      prop_keys = prop.keys()
+      for i in prop_keys:
+        prop_type = self.extract_type(prop[i])
+        prop_set[i] =prop_type
+      ret = self.add_type(ret, prop_set)
+    if "additionalProperties" in param_data:
+      add_prop = self.extract_type(param_data["properties"])
+      ret = self.add_type(ret, prop)
+    if "allOf" in param_data:
+      all_of = self.extract_type(param_data["allOf"])
+      ret = self.add_type(ret, all_of)
+    # TODO : handle oneOf if we find oneOf in method
+    #if "oneOf" in param_data:
+    #  one_of = self.extract_type(param_data["oneOf"])
+    #  ret = self.add_type(ret, one_of)
+    # TODO : handle anyOf if we find oneOf in method
+    #if "anyOf" in param_data:
+    #  any_of = self.extract_type(param_data["anyOf"])
+    #  ret = self.add_type(ret, any_of)
     return ret
 
   def get_produce(self, method):
